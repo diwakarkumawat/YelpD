9a8d35cafdad1ac4ade64279822d352ee40a6319
dkumawat <diwakar_kumawat@intuit.com>
2017-04-09 15:17:18 -0700

    implemented search

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, FiltersViewControllerDelegate {
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, FiltersViewControllerDelegate, UISearchBarDelegate {
     
     var businesses: [Business]!
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var searchBar: UISearchBar!
+    var myLabel : UILabel!
+    
+    let data = ["New York, NY", "Los Angeles, CA", "Chicago, IL", "Houston, TX",
+                "Philadelphia, PA", "Phoenix, AZ", "San Diego, CA", "San Antonio, TX",
+                "Dallas, TX", "Detroit, MI", "San Jose, CA", "Indianapolis, IN",
+                "Jacksonville, FL", "San Francisco, CA", "Columbus, OH", "Austin, TX",
+                "Memphis, TN", "Baltimore, MD", "Charlotte, ND", "Fort Worth, TX"]
+    
+   var filteredData: [String]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +32,58 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         tableView.dataSource = self
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 120
+        
+        searchBar = UISearchBar()
+        searchBar.delegate = self
+        searchBar.sizeToFit()
+        
+        // add shadow
+        searchBar.layer.shadowColor = UIColor.black.cgColor
+        searchBar.layer.shadowOpacity = 0.5
+        searchBar.layer.masksToBounds = false
+        
+        // hide cancel button
+        searchBar.showsCancelButton = true
+        
+        // hide bookmark button
+        searchBar.showsBookmarkButton = false
+        
+        // set Default bar status.
+        searchBar.searchBarStyle = UISearchBarStyle.default
+        
+        // set title
+        searchBar.prompt = "Title"
+        
+        // set placeholder
+        searchBar.placeholder = "Input text"
+        
+        // change the color of cursol and cancel button.
+        searchBar.tintColor = UIColor.red
+        
+        // hide the search result.
+        searchBar.showsSearchResultsButton = false
+        
+        // add searchBar to the view.
+        self.view.addSubview(searchBar)
+        
+        // make UITextField
+        myLabel = UILabel()
+        myLabel.center = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height/2)
+        myLabel.text = ""
+        myLabel.layer.borderWidth = 1.0
+        myLabel.layer.borderColor = UIColor.gray.cgColor
+        myLabel.layer.cornerRadius = 10.0
+        
+        // add the label to the view.
+        self.view.addSubview(myLabel)
+        self.navigationController?.navigationBar.addSubview(myLabel)
+        
+        // the UIViewController comes with a navigationItem property
+        // this will automatically be initialized for you if when the
+        // view controller is added to a navigation controller's stack
+        // you just need to set the titleView to be the search bar
+        navigationItem.titleView = searchBar
+        
         
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
@@ -35,6 +98,52 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
             
             }
         )
+    }
+    
+    // called whenever text is changed.
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        myLabel.text = searchText
+    }
+    
+    // called when cancel button is clicked
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+
+        searchBar.text = ""
+        let searchTerm = searchBar.text ?? ""
+        
+        Business.searchWithTerm(term: searchTerm, completion: { (businesses: [Business]?, error: Error?) -> Void in
+            
+            self.businesses = businesses
+            
+            self.tableView.reloadData()
+            if let businesses = businesses {
+                for business in businesses {
+                    print(business.name!)
+                    print(business.address!)
+                }
+            }
+        })
+    }
+    
+    // called when search button is clicked
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // myLabel.text = "Searching"
+        //searchBar.text = ""
+        
+        
+        let searchTerm = searchBar.text ?? ""
+        
+        Business.searchWithTerm(term: searchTerm, completion: { (businesses: [Business]?, error: Error?) -> Void in
+            
+            self.businesses = businesses
+            self.tableView.reloadData()
+            if let businesses = businesses {
+                for business in businesses {
+                    print(business.name!)
+                    print(business.address!)
+                }
+            }
+        })
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
