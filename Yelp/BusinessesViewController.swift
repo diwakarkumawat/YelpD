11a1a38fc99e39c5a860990ce5f3997b47d95807
dkumawat <diwakar_kumawat@intuit.com>
2017-04-09 22:15:48 -0700

    yelp submission

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -17,14 +17,6 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
     var searchBar: UISearchBar!
     var myLabel : UILabel!
     
-    let data = ["New York, NY", "Los Angeles, CA", "Chicago, IL", "Houston, TX",
-                "Philadelphia, PA", "Phoenix, AZ", "San Diego, CA", "San Antonio, TX",
-                "Dallas, TX", "Detroit, MI", "San Jose, CA", "Indianapolis, IN",
-                "Jacksonville, FL", "San Francisco, CA", "Columbus, OH", "Austin, TX",
-                "Memphis, TN", "Baltimore, MD", "Charlotte, ND", "Fort Worth, TX"]
-    
-   var filteredData: [String]!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
